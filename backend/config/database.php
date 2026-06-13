be30d1e56d6109284256a67d61bcdc0b531b6036
RPAN <watchern@126.com>
2026-06-13 12:58:01 +0800

    修复.env文件格式兼容Symfony Dotenv
    问题：Symfony Dotenv不支持section格式如[APP]、[DATABASE]
    
    修复：
    - 去掉所有section标记，使用简单的KEY=value格式
    - 变量名改为大写下划线格式：DATABASE_TYPE、DATABASE_SQLITE_PATH等
    - 更新database.php配置匹配新的环境变量名

--- backend/config/database.php
+++ backend/config/database.php
@@ -4,20 +4,20 @@ use think\facade\Env;
 // 数据库配置
 return [
     // 默认数据库连接
-    'default' => Env::get('database.type', 'sqlite'),
+    'default' => Env::get('DATABASE_TYPE', 'sqlite'),
 
     // 数据库连接信息
     'connections' => [
         'mysql' => [
             'type' => Env::get('DATABASE_TYPE', 'mysql'),
-            'hostname' => Env::get('database_hostname', '127.0.0.1'),
-            'database' => Env::get('database_database', 'moive_app'),
-            'username' => Env::get('database_username', 'root'),
-            'password' => Env::get('database_password', ''),
-            'hostport' => Env::get('database_hostport', '3306'),
+            'hostname' => Env::get('DATABASE_HOSTNAME', '127.0.0.1'),
+            'database' => Env::get('DATABASE_DATABASE', 'moive_app'),
+            'username' => Env::get('DATABASE_USERNAME', 'root'),
+            'password' => Env::get('DATABASE_PASSWORD', ''),
+            'hostport' => Env::get('DATABASE_HOSTPORT', '3306'),
             'params' => [],
-            'charset' => Env::get('database_charset', 'utf8mb4'),
-            'prefix' => Env::get('database_prefix', ''),
+            'charset' => Env::get('DATABASE_CHARSET', 'utf8mb4'),
+            'prefix' => Env::get('DATABASE_PREFIX', ''),
             'deploy' => 0,
             'rw_separate' => false,
             'master_num' => 1,
@@ -28,8 +28,8 @@ return [
         ],
         'sqlite' => [
             'type' => 'sqlite',
-            'database' => Env::get('database.sqlite_path', dirname(__DIR__, 2) . '/database/database.sqlite'),
-            'prefix' => Env::get('database_prefix', ''),
+            'database' => Env::get('DATABASE_SQLITE_PATH', dirname(__DIR__, 2) . '/database/database.sqlite'),
+            'prefix' => Env::get('DATABASE_PREFIX', ''),
             'charset' => 'utf8',
         ],
     ],
